01e33a55c5e2c930dae2d8abdb2bddd0314afae4
Jyo <jyoshim92@gmail.com>
2016-02-04 01:34:28 -0800

    account for level

--- javascripts/mundus.js
+++ javascripts/mundus.js
@@ -3,6 +3,7 @@ var appr = 167;
 var thief = 0.11;
 var shad = 0.12;
 
+var l, vr, el;
 var m, mb, sd, sdb, cc, cd, d;
 var bd;
 var b1, b2, b3, b4;
@@ -13,6 +14,18 @@ function optimizeMundusStone() {
     var mundusForm = document.forms["mundusform"];
     var bestStone = "";
 
+    l = mundusForm.elements["level"].value || 1;
+    el = parseInt(l);
+    vr = mundusForm.elements["vr"].checked;
+    if (vr) {
+        el += 50;
+        l = "VR" + l;
+    } else
+        l = "Level " + l;
+
+    mage = 20 * el;
+    appr = Math.floor(2.54 * el);
+    
     m = parseInt(mundusForm.elements["magicka"].value) || 0;
     mb = parseInt(mundusForm.elements["magickabuff"].value) / 100 || 0;
     sd = parseInt(mundusForm.elements["spelldamage"].value) || 0;
@@ -82,25 +95,25 @@ function populateMath() {
 "</br>Mundus Stone Bonus: Buff * (1 + <b>D</b>ivines) * (1 + <b>P</b>ercent<b>I</b>ncrease)" +
 "</br>" +
 "</br><b>The Mage</b>" +
-"</br>At VR16, The Mage provides <b>" + mage + " Magicka</b>" +
+"</br>At " + l + ", The Mage provides <b>" + mage + " Magicka</b>" +
 "</br>For you: " + mage + " * (1 + " + d + ") * (1 + " + mb + ") = <b>" + b1 + "</b>" +
 "</br>New damage: (" + m + " + " + b1 + " + 10.46 * " + sd + ") * (1 + " + cd + " * " + cc + ") = <b>" + bd1 + "</b>" +
 "</br>Damage difference: " + bd1 + " - " + bd + " = <b>" + d1 + "</b>" +
 "</br>" +
 "</br><b>The Apprentice</b>" +
-"</br>At VR16, The Apprentice provides <b>" + appr + " Spell Damage</b>" +
+"</br>At " + l + ", The Apprentice provides <b>" + appr + " Spell Damage</b>" +
 "</br>For you: " + appr + " * (1 + " + d + ") * (1 + " + sdb + ") = <b>" + b2 + "</b>" +
 "</br>New damage: (" + m + " + 10.46 * (" + sd + " + " + b2 + ")) * (1 + " + cd + " * " + cc + ") = <b>" + bd2 + "</b>" +
 "</br>Damage difference: " + bd2 + " - " + bd + " = <b>" + d2 + "</b>" +
 "</br>" +
 "</br><b>The Thief</b>" +
-"</br>At VR16, The Thief provides <b>" + roundTo2(thief * 100) + "% Crit Chance</b>" +
+"</br>At " + l + ", The Thief provides <b>" + roundTo2(thief * 100) + "% Crit Chance</b>" +
 "</br>For you: " + roundTo2(thief * 100) + " * (1 + " + d + ") = <b>" + roundTo2(b3 * 100) + "%</b>" +
 "</br>New damage: (" + m + " + 10.46 * " + sd + ") * (1 + " + cd + " * (" + cc + " + " + b3 + ")) = <b>" + bd3 + "</b>" +
 "</br>Damage difference: " + bd3 + " - " + bd + " = <b>" + d3 + "</b>" +
 "</br>" +
 "</br><b>The Shadow</b>" +
-"</br>At VR16, The Shadow provides <b>" + roundTo2(shad * 100) + "% Crit Damage</b>" +
+"</br>At " + l + ", The Shadow provides <b>" + roundTo2(shad * 100) + "% Crit Damage</b>" +
 "</br>For you: " + roundTo2(shad * 100) + " * (1 + " + d + ") = <b>" + roundTo2(b4 * 100) + "%</b>" +
 "</br>New damage: (" + m + " + 10.46 * " + sd + ") * (1 + (" + cd + " + " + b4 + ") * " + cc + ") = <b>" + bd4 + "</b>" +
 "</br>Damage difference: " + bd4 + " - " + bd + " = <b>" + d4 + "</b>";
