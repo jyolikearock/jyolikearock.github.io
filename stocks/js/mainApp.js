e42b344584dd4289bc3c11af7b1a44064ed30d83
Jyo <jyoshim92@gmail.com>
2018-10-25 21:36:44 -0700

    move ng-app into html tag; reference views directory from current path

--- stocks/js/mainApp.js
+++ stocks/js/mainApp.js
@@ -3,7 +3,7 @@ var app = angular.module("mainApp", ["ngRoute"]);
 app.config(["$routeProvider", function ($routeProvider) {
   $routeProvider
     .when("/home", {
-      templateUrl: "views/home.html"
+      templateUrl: "./views/home.html"
     })
     .otherwise({ redirectTo: "/" });
 }]);
