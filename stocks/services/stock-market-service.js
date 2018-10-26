47a28158c7c2702ea585087ceb6bf1ce6a3dadab
Jiyong Shim <jyolikearock@github.com>
2018-10-25 23:56:38 -0700

    re order functions in stock market service

--- stocks/services/stock-market-service.js
+++ stocks/services/stock-market-service.js
@@ -3,20 +3,7 @@ app.service("stockMarketService", function($http) {
   var baseEndpoint = "https://api.iextrading.com/1.0/stock/";
   var listSectorsUrl = baseEndpoint + "market/sector-performance";
 
-  this.listSectors = function() {
-    httpGet(listSectorsUrl).then(
-      function(resp) {
-        console.log("Got response from listSectors call: ", resp);
-      },
-      function(error) {
-        console.log("Got error from listSectors call: ", error);
-      });
-  }
-
-  this.httpGet = function(url) {
-    $http.get(url).then(successCallBack, errorCallBack);
-  }
-
+  // utility methods
   this.successCallBack = function(resp) {
     console.log("Got response from server: ", resp);
     return resp;
@@ -25,5 +12,20 @@ app.service("stockMarketService", function($http) {
   this.errorCallBack = function(error) {
     console.log("Got error from server: ", error);
     return error;
+  }
+
+  this.httpGet = function(url) {
+    $http.get(url).then(successCallBack, errorCallBack);
+  }
+
+  // stock market api
+  this.listSectors = function() {
+    httpGet(listSectorsUrl).then(
+      function(resp) {
+        console.log("Got response from listSectors call: ", resp);
+      },
+      function(error) {
+        console.log("Got error from listSectors call: ", error);
+      });
   }
 });
