3b28364dc2c4325c528803d48c77ff2638008f9b
Jiyong Shim <jyolikearock@github.com>
2018-10-25 23:55:09 -0700

    fix syntax error

--- stocks/controllers/main-controller.js
+++ stocks/controllers/main-controller.js
@@ -3,4 +3,4 @@ app.controller("mainController", function($scope, stockMarketService) {
   // populate sectors
   console.log("Getting market sectors");
   stockMarketService.listSectors();
-}]);
+});
