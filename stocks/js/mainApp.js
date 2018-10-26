b75ef2d096e419b6cb1efbb0909727f6de5601d7
Jyo <jyoshim92@gmail.com>
2018-10-25 21:27:22 -0700

    try integrating with angular

---
+++ stocks/js/mainApp.js
@@ -0,0 +1,9 @@
+var app = angular.module("mainApp", ["ngRoute"]);
+
+app.config(["$routeProvider", function ($routeProvider) {
+  $routeProvider
+    .when("/home", {
+      templateUrl: "views/home.html"
+    })
+    .otherwise({ redirectTo: "/" });
+}]);
