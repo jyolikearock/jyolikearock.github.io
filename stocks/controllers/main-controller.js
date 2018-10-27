89f19b03394eb68412e9725cdbcc43b8c9b87f0d
Jiyong Shim <jyolikearock@github.com>
2018-10-26 23:43:04 -0700

    highlight home nav pill correctly

--- stocks/controllers/main-controller.js
+++ stocks/controllers/main-controller.js
@@ -4,6 +4,10 @@ app.controller("mainController", function($scope, stockMarketService) {
 
   $scope.pageInfo = pageInfo;
 
+  $scope.resetCurrentPage = function() {
+    pageInfo.currentPage = "Home";
+  }
+
   // populate sectors
   console.log("Getting market sectors");
   $scope.sectors = [];
