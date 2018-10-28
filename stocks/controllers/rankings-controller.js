a490d2db51a41931323f4337c9cedff639bf35fb
Jiyong Shim <jyolikearock@github.com>
2018-10-28 11:48:51 -0700

    refine rating evaluation

--- stocks/controllers/rankings-controller.js
+++ stocks/controllers/rankings-controller.js
@@ -21,6 +21,9 @@ app.controller("rankingsController", function($scope, stockMarketService) {
             console.log("No valid chart found for symbol: ", symbol);
           }
         });
+
+        normalize(ratings);
+
         ratings.sort(function(a, b) {
           return b.overall - a.overall;
         });
@@ -37,24 +40,34 @@ app.controller("rankingsController", function($scope, stockMarketService) {
     rating.symbol = symbol;
     rating.consistency = evaluateConsistency(chart);
     rating.growth = evaluateGrowth(chart);
-    rating.overall = rating.consistency + rating.growth;
 
     return rating;
   }
 
   function evaluateConsistency(chart) {
-    var rating = 0;
+    var rating = 100;
 
-    var previousPrice = -1;
+    var previousPrice = undefined;
+    var previousGrowth = undefined;
     chart.forEach(
       function(dataPoint) {
         let price = dataPoint.close;
-        if (previousPrice >= 0) {
-          if (price > previousPrice) {
-            rating++;
-          }
+        if (previousPrice === undefined) {
+          previousPrice = price;
+          return;
         }
+
+        let growth = getPercentDiff(previousPrice, price);
+        if (previousGrowth === undefined) {
+          previousGrowth = growth;
+          return;
+        }
+
+        let diffInGrowth = Math.abs(growth - previousGrowth);
+        rating = rating - diffInGrowth;
+
         previousPrice = price;
+        previousGrowth = growth;
       }
     );
 
@@ -65,7 +78,40 @@ app.controller("rankingsController", function($scope, stockMarketService) {
     var start = chart[0].close;
     var end = chart[chart.length - 1].close;
 
-    return (end / start - 1) * 100;
+    return getPercentDiff(start, end);
+  }
+
+  function normalize(ratings) {
+
+    // normalize consistency
+    normalizeField("consistency");
+
+    // normalize growth
+    normalizeField("growth");
+
+    // average the two to compute overall rating
+    ratings.forEach(function(rating) {
+      rating.overall = (rating.consistency + rating.growth) / 2;
+    });
+  }
+
+  function normalizeField(ratings, fieldName) {
+
+    // sort field in ascending order
+    ratings.sort(function(a, b) {
+      return a[fieldName] - b[fieldName];
+    });
+
+    let dataPointsPerPercentile = Math.ceil(ratings.length * 1.0 / 1000);
+    for (let p = 0; p < 1000; p++) {
+      for (let i = p * dataPointsPerPercentile; i < dataPointsPerPercentile; i++) {
+        ratings[i][fieldName] = (p + 1) / 10.0;
+      }
+    }
+  }
+
+  function getPercentDiff(a, b) {
+    return (b * 1.0 / a - 1) * 100;
   }
 
 });
